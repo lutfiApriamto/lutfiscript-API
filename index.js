ee77b2148100e3f0025bb2ee6e6f7c581e756ed8
LutfiApriamto <afriantolutfi@gmail.com>
2024-07-02 13:27:14 +0700

    memperbaiki

--- index.js
+++ index.js
@@ -9,30 +9,22 @@ import { ModulesRouter } from './routes/modules.js';
 import { AdminRouter } from './routes/admin.js';
 
 const app = express();
-app.use(express.json());
 app.use(cors({
     origin: "https://lutfi-script-client.vercel.app",
-    methods : ["GET", "POST", "PATCH", "DELETE"],
-    credentials : true
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+    optionsSuccessStatus: 200
 }));
 
-app.use(cookieParser())
-app.use('/admin', AdminRouter )
-app.use('/auth', UserRouter)
-app.use('/auth/:id', UserRouter)
-app.use('/modules',ModulesRouter )
-app.use('/modules/:id',ModulesRouter )
+app.use(express.json());
+app.use(cookieParser());
 
-// mongoose.connect('mongodb://127.0.0.1:27017/lutfiscript')
-//     .then(() => {
-//     console.log("Connected to MongoDB");
-//     app.listen(process.env.PORT, () => {
-//         console.log(`Server is running on port ${process.env.PORT}`);
-//     });
-// }).catch(err => {
-//     console.error("Connection error", err);
-// });
+app.use('/admin', AdminRouter);
+app.use('/auth', UserRouter);
+app.use('/auth/:id', UserRouter);
+app.use('/modules', ModulesRouter);
+app.use('/modules/:id', ModulesRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log(`app is running`)
-})
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
