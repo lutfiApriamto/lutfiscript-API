6eadf94e8f2c87b1eafb754117040e9ea1990d35
LutfiApriamto <afriantolutfi@gmail.com>
2024-07-02 13:35:11 +0700

    memperbaiki lagi

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ import { ModulesRouter } from './routes/modules.js';
 import { AdminRouter } from './routes/admin.js';
 
 const app = express();
+
+// Log request origin
+app.use((req, res, next) => {
+    console.log('Request origin:', req.headers.origin);
+    next();
+});
+
 app.use(cors({
     origin: "https://lutfi-script-client.vercel.app",
     methods: ["GET", "POST", "PATCH", "DELETE"],
@@ -16,6 +23,7 @@ app.use(cors({
     optionsSuccessStatus: 200
 }));
 
+app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,6 +33,13 @@ app.use('/auth/:id', UserRouter);
 app.use('/modules', ModulesRouter);
 app.use('/modules/:id', ModulesRouter);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+    });
